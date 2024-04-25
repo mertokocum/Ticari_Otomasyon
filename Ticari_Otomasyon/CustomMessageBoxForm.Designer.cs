8cf2b1a8592d25d7311bb1eb2cefd79a4376a474
mertokocum <hmrtkocum@gmail.com>
2024-04-26 01:28:31 +0300

    ileride kullanılabilir custommessageboxform

---
+++ Ticari_Otomasyon/CustomMessageBoxForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Ticari_Otomasyon
+{
+    partial class CustomMessageBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new DevExpress.XtraEditors.LabelControl();
+            this.messageLabel = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Location = new System.Drawing.Point(114, 38);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(63, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "labelControl1";
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Location = new System.Drawing.Point(114, 90);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(63, 13);
+            this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "labelControl2";
+            // 
+            // CustomMessageBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "CustomMessageBoxForm";
+            this.Text = "CustomMessageBoxForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl titleLabel;
+        private DevExpress.XtraEditors.LabelControl messageLabel;
+    }
+}
---
+++ Ticari_Otomasyon/CustomMessageBoxForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ticari_Otomasyon
+{
+    public partial class CustomMessageBoxForm : Form
+    {
+        public CustomMessageBoxForm(string title, string message)
+        {
+            InitializeComponent();
+            titleLabel.Text = title;
+            messageLabel.Text = message;
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+            Close();
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+            Close();
+        }
+    }
+}
