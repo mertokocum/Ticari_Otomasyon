0a132f2f2a83281e1bac2da5c5378e15eeb36b17
mertokocum <hmrtkocum@gmail.com>
2025-03-17 05:56:05 +0300

    Edit

--- Ticari_Otomasyon/FrmMail.Designer.cs
+++ Ticari_Otomasyon/FrmMail.Designer.cs
@@ -28,29 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMail));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMesaj = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnGonder = new DevExpress.XtraEditors.SimpleButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TxtKonu = new DevExpress.XtraEditors.TextEdit();
+            this.TxtMailAdres = new DevExpress.XtraEditors.TextEdit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKonu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMailAdres.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 112);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 153);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 23);
+            this.label1.Size = new System.Drawing.Size(93, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Mail Adresi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Konu:";
+            // 
+            // TxtMesaj
+            // 
+            this.TxtMesaj.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMesaj.Location = new System.Drawing.Point(134, 217);
+            this.TxtMesaj.Name = "TxtMesaj";
+            this.TxtMesaj.Size = new System.Drawing.Size(351, 108);
+            this.TxtMesaj.TabIndex = 4;
+            this.TxtMesaj.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mesaj:";
+            // 
+            // BtnGonder
+            // 
+            this.BtnGonder.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGonder.Appearance.Options.UseFont = true;
+            this.BtnGonder.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.BtnGonder.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.BtnGonder.Location = new System.Drawing.Point(368, 348);
+            this.BtnGonder.Name = "BtnGonder";
+            this.BtnGonder.Size = new System.Drawing.Size(160, 36);
+            this.BtnGonder.TabIndex = 0;
+            this.BtnGonder.Text = "Gönder";
+            this.BtnGonder.Click += new System.EventHandler(this.BtnGonder_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Ticari_Otomasyon.Properties.Resources.mailbg2;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(540, 132);
+            this.panel1.TabIndex = 6;
+            // 
+            // TxtKonu
+            // 
+            this.TxtKonu.Location = new System.Drawing.Point(134, 185);
+            this.TxtKonu.Name = "TxtKonu";
+            this.TxtKonu.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtKonu.Properties.Appearance.Options.UseFont = true;
+            this.TxtKonu.Size = new System.Drawing.Size(351, 26);
+            this.TxtKonu.TabIndex = 3;
+            // 
+            // TxtMailAdres
+            // 
+            this.TxtMailAdres.Location = new System.Drawing.Point(134, 153);
+            this.TxtMailAdres.Name = "TxtMailAdres";
+            this.TxtMailAdres.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMailAdres.Properties.Appearance.Options.UseFont = true;
+            this.TxtMailAdres.Size = new System.Drawing.Size(351, 26);
+            this.TxtMailAdres.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(126, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(363, 45);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mail Gönderme Paneli";
             // 
             // FrmMail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 457);
+            this.ClientSize = new System.Drawing.Size(540, 396);
+            this.Controls.Add(this.BtnGonder);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtMesaj);
+            this.Controls.Add(this.TxtKonu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtMailAdres);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "FrmMail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmMail";
             this.Load += new System.EventHandler(this.FrmMail_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKonu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMailAdres.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +166,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.TextEdit TxtMailAdres;
+        private DevExpress.XtraEditors.TextEdit TxtKonu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox TxtMesaj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.SimpleButton BtnGonder;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Ticari_Otomasyon/FrmMail.cs
+++ Ticari_Otomasyon/FrmMail.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+
 
 namespace Ticari_Otomasyon
 {
@@ -19,7 +23,30 @@ namespace Ticari_Otomasyon
         public string mail;
         private void FrmMail_Load(object sender, EventArgs e)
         {
-            label1.Text = mail;
+            TxtMailAdres.Text = mail;
+        }
+        
+
+        private void BtnGonder_Click(object sender, EventArgs e)
+        {
+            string v = ConfigurationManager.AppSettings["SMTP_EMAIL"];
+            string smtpEmail = v;
+            string v1 = ConfigurationManager.AppSettings["SMTP_PASSWORD"];
+            string smtpPassword = v1;
+
+            MailMessage mesaj = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.UseDefaultCredentials = false;
+            istemci.Credentials = new NetworkCredential(v,v1);
+            istemci.Port = 587;
+            istemci.Host = "smtp.gmail.com";
+            istemci.EnableSsl = true;
+            
+            mesaj.To.Add(TxtMailAdres.Text);
+            mesaj.From = new MailAddress(v);
+            mesaj.Subject = TxtKonu.Text;
+            mesaj.Body = TxtMesaj.Text;
+            istemci.Send(mesaj);
         }
     }
 }
